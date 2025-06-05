e3b624daa3c44768e564f225b5821975e1c37096
marjonakenjayeva <marjonakenjayeva62@gmail.com>
2025-06-05 15:05:14 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const ism = prompt("Ismingizni kiriting");
+const familya = prompt("Familyangizni kiriting");
+let yosh = prompt("Yoshingizni kiriting");
+let fan = prompt("Yoqtirgan faningizni kiriting");
+let tuman = prompt("Yashaydigan tumaninmgizni kiriting");
+let hobby = prompt("Sevimli mashgulotingizni kiriting");
+let shahar = prompt("Sevimli shahringizni kiriting")
+
+console.log (`
+  Ism: ${ism} 
+  Familya: ${familya}
+  Yosh: ${yosh}
+  Fan: ${fan}
+  Yashash joy: ${tuman}
+  Sevimli Mahgulot: ${hobby}
+  Sevmli Shahar: ${shahar}
+`)
